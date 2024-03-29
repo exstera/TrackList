10528a81dde7536ce44bb50f1872000c8a5b8b69
gleb <belkov.glebka@yandex.ru>
2024-03-29 15:20:17 +0300

    add TrackDetailsVC

---
+++ TrackList/TrackDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrackDetailsViewController.swift
+//  TrackList
+//
+//  Created by Глеб Бельков on 29.03.2024.
+//
+
+import UIKit
+
+class TrackDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
